43bebe23b8475a2e9e85aae0ca4d218ca2df67a7
Jordan Heale <webmaster@pridit.co.uk>
2021-02-02 03:23:06 +0000

    Ignore SSL again because the certificate keeps expiring

--- notify.py
+++ notify.py
@@ -17,7 +17,7 @@ def config_exists():
     """
     with open('config.ini') as file:
         parser.read_file(file)
-        
+
 def get_collections():
     """
     Make the HTTP request and parses the resulting JSON
@@ -27,31 +27,31 @@ def get_collections():
                'X-country': 'gb',
                'X-email': parser.get('athomeapp', 'email')}
 
-    response = requests.get(parser.get('athomeapp', 'url'), headers=headers)
+    response = requests.get(parser.get('athomeapp', 'url'), headers=headers, verify=False)
 
     if response.status_code == 200:
         collections = json.loads(response.content.decode('utf-8'))
-        
+
         ready = []
         ignored = []
-        
+
         tomorrow = datetime.now() + timedelta(days=1)
-        
+
         for collection in collections.get('userrounds'):
             container = collection.get('containername')
-            
+
             if container == 'Chargeable garden waste bin' and not args.garden:
                 ignored.append(container)
                 continue
-                
+
             if container in legacy and not args.old:
                 ignored.append(container)
                 continue
-            
+
             try:
                 soonest = collection.get('nextcollectiondates', []).pop(0).get('datestring')
                 pickup = datetime.strptime(soonest, '%d %m %Y %I:%M')
-                
+
                 if pickup.date() == tomorrow.date():
                     ready.append(container)
             except IndexError:
@@ -60,15 +60,15 @@ def get_collections():
             except AttributeError:
                 # 'None' (suspended collections?)
                 pass
-                
+
         if ready:
             print('{} collection(s) tomorrow ({})'.format(len(ready), ', '.join(map(str, ready))))
-            
+
             if ignored:
                 print('{} ignored ({})'.format(len(ignored), ', '.join(map(str, ignored))))
-            
+
             notification(ready)
-            
+
 def notification(collections):
     """
     Notify user of their collection day along with waste
@@ -80,9 +80,9 @@ def notification(collections):
             'user': parser.get('pushover', 'user_key'),
             'message': '\r\n'.join(map(str, collections)),
             'title': 'Your collection day is tomorrow'}
-    
+
     requests.post('https://api.pushover.net/1/messages.json', data)
-    
+
 if __name__ == '__main__':
     parser = ConfigParser(os.environ)
 
@@ -91,10 +91,10 @@ if __name__ == '__main__':
     arguments.add_argument('-o', '--old', help='include old collections (small boxes)', action='store_true')
 
     args = arguments.parse_args()
-    
+
     legacy = [
         'Glass box', 'Paper & Cardboard', 'Plastic and Cans'
     ]
-    
+
     config_exists()
     get_collections()
